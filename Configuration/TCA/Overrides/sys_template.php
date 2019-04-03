9ff338ec07eb190475d1a2182d0dd1e61daab83f
Sybille Peters <sypets@gmx.de>
2019-04-03 15:09:28 +0200

    Move static includes instead of ext_typoscript_setup.txt

---
+++ Configuration/TCA/Overrides/sys_template.php
@@ -0,0 +1,7 @@
+<?php
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+    'ods_osm_tt_address',
+    'Configuration/TypoScript/',
+    'Template OpenStreetMap with tt_address'
+);
